39695c905f8c229d0e0f15a14c413ce9ba879094
SachaRoland <94817916+SachaRoland@users.noreply.github.com>
2021-12-14 19:52:39 -0800

    Update 3. data analysis.R

--- 3. data analysis.R
+++ 3. data analysis.R
@@ -5,3 +5,21 @@
  # explain what steps would need to be taken to actually measure biodiversity. 
 # How would we deal with all the taxa? 
 # what kind of fucnction? --> Shannon's index. 
+
+
+# Use objects used to create the quadrants to create a data frame. 
+# Since the grid is the same for both amphibians and buiding permit data points, the order of grid cell 
+# is the same. The the numbers of data point for each variables are thus sort of paired within each quadrant.
+
+# Create a data frame using tibble()
+grid.data.frame <- tibble(build.grid$count, amphi.grid$count)
+
+#Should be able to get a bird.grid$count that can be combine to amphibians. 
+# all.taxa and build.grid would then become new data frame for stat analysis.
+
+# get plot with  number building permits as the explanatory variable and 
+# amphibian occurrence as the response variable. 
+plot(grid.data.frame)
+
+# Do a mini correlation test
+cor(amphi.grid$count, build.grid$count, method = "pearson") 
