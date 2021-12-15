7024041489fb631227479fe2476c08798c37f6c7
SachaRoland <94817916+SachaRoland@users.noreply.github.com>
2021-12-14 19:22:07 -0800

    Update 3. data analysis.R

--- 3. data analysis.R
+++ 3. data analysis.R
@@ -1,1 +1,7 @@
+# In order to see wether buidling permits is negatively correlated with amphibian species occurence
+# We need to run a correlation analysis using Pearson's correlation coefficient. 
 
+
+ # explain what steps would need to be taken to actually measure biodiversity. 
+# How would we deal with all the taxa? 
+# what kind of fucnction? --> Shannon's index. 
